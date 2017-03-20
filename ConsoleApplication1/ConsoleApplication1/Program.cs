9f82665c993c3bad65051f27cfb50e02f7a01ef8
devikard <devikad@cybage.com>
2017-03-20 11:43:55 +0530

    Feature1 commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Master Branch");
+            Console.WriteLine("Feature1 Branch");
         }
     }
 }
